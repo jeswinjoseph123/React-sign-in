883d389090d485c525964dfd0961a1f742f7892c
jeswinjoseph123 <jeswinjoseph08@gmail.com>
2020-10-21 19:55:11 +0530

    Updates

---
+++ precache-manifest.37f3bce8fb095299ad21e6aacfb2a258.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b5ff03f1a774b7e53c67080a110646",
+    "url": "/React-sign-in/index.html"
+  },
+  {
+    "revision": "16d5bce4e9431c85446e",
+    "url": "/React-sign-in/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7ec6163bba72520f1d6e",
+    "url": "/React-sign-in/static/css/main.59c8aaf9.chunk.css"
+  },
+  {
+    "revision": "16d5bce4e9431c85446e",
+    "url": "/React-sign-in/static/js/2.78e76c8a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-sign-in/static/js/2.78e76c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec6163bba72520f1d6e",
+    "url": "/React-sign-in/static/js/main.1e6f3191.chunk.js"
+  },
+  {
+    "revision": "c13ab5497c19c0e798d7",
+    "url": "/React-sign-in/static/js/runtime-main.e831224a.js"
+  }
+]);
